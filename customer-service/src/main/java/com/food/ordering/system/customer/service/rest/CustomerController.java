896c4e6fc2dd54ad5bb2829f1991593d18862db7
Eder <nwoswo@gmail.com>
2025-08-20 12:43:43 -0500

    avance

---
+++ customer-service/src/main/java/com/food/ordering/system/customer/service/rest/CustomerController.java
@@ -0,0 +1,21 @@
+// package com.food.ordering.system.customer.service.rest;
+
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// @RequestMapping("/api/v1/customers")
+// public class CustomerController {
+
+// @GetMapping("/health")
+// public ResponseEntity<String> health() {
+// return ResponseEntity.ok("Customer Service is running!");
+// }
+
+// @GetMapping
+// public ResponseEntity<String> getCustomers() {
+// return ResponseEntity.ok("Customer Service - Get Customers endpoint");
+// }
+// }
